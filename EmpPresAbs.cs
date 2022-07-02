f93141c485ce5aca485c7e54b92e93be7fce71e6
Nitish Mehta <nkrmehta09@gmail.com>
2022-07-02 13:54:09 +0530

    EmpPresAbs

---
+++ EmpPresAbs.cs
@@ -0,0 +1,21 @@
+﻿
+// Calculate Daily Employee Wage
+
+ int WagePerHrs = 20;
+ int FullTime = 8;
+ int NoWork = 0;
+ Random ran = new Random();
+
+int AttenCheck = ran.Next(0, 9);
+
+if (AttenCheck == FullTime)
+{
+    Console.WriteLine("Emp is Present");
+    Console.WriteLine("EmpWage : " + (WagePerHrs * FullTime));
+}
+else
+{
+    Console.WriteLine("Emp is Absent");
+    Console.WriteLine("EmpWage : " + (WagePerHrs * NoWork));
+    Console.ReadLine();
+}
