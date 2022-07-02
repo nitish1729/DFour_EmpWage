f50420e49e611392480f4a4d59ab7541eb76dc8a
Nitish Mehta <nkrmehta09@gmail.com>
2022-07-02 15:29:29 +0530

    EmpMonthlywages

---
+++ EmpTotalMonthlyWages.cs
@@ -0,0 +1,34 @@
+﻿
+const int WagePerHrs = 20;
+const int WorkingDay = 100;
+const int FullTime = 8;
+const int PartTime = 4;
+const int NoWork = 0, MaxWorkingHrs = 100;
+
+int totalWorkingDay = 0, totalEmpHrs = 0;
+while (totalEmpHrs <= MaxWorkingHrs && totalWorkingDay < WorkingDay)
+{
+	totalWorkingDay++;
+	Random random = new Random();
+	int AttenCheck = random.Next(6, 9);
+
+	switch (AttenCheck)
+	{
+		case 8:
+			Console.WriteLine("Emp is present ");
+			Console.WriteLine("Total Wage : " + (WagePerHrs* WorkingDay * FullTime));
+			break;
+						
+			case 4:
+			Console.WriteLine("Emp is HalfDayWork ");
+			Console.WriteLine("Total Wage : " + (WagePerHrs* WorkingDay * PartTime));
+			break;
+
+		default:
+
+			Console.WriteLine("Emp is Absent  ");
+			Console.WriteLine("Total Wage : " + (WagePerHrs* WorkingDay * NoWork));
+			break;
+	}
+
+}
---
+++ EmpwagesBySwitchCae.cs
@@ -0,0 +1,33 @@
+﻿// Solving using Switch Case Statement
+
+string Welcome = "Welcome to Employee Wage Computation";
+ Console.WriteLine(Welcome);
+
+
+Console.WriteLine("...........................................");
+
+ int WagePerHrs = 20;
+ int FullTime = 8;
+ int PartTime = 4;
+ int NoWork = 0;
+Random random = new Random();
+int AttenCheck = random.Next(0, 9);
+
+switch (AttenCheck)
+{
+	case 8:
+		Console.WriteLine("Emp is present ");
+		Console.WriteLine("Emp Total Wage : " + (WagePerHrs * FullTime));
+		break;
+
+	case 4:
+		Console.WriteLine("Emp Part Time Wages ");
+		Console.WriteLine("Emp Part Time Wages is: " + (WagePerHrs * PartTime));
+		break;
+
+	default:
+
+		Console.WriteLine("Emp is Absent  ");
+		Console.WriteLine("Total Wage : " + (WagePerHrs * NoWork));
+		break;
+}
